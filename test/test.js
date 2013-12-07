7f548b1011d8c74ffa6db34f333c54ec01ba96e1
jesse keane <me@jessekeane.me>
2013-12-06 16:45:47 -0800

    fixed reverselookup

--- test/test.js
+++ test/test.js
@@ -10,3 +10,5 @@ var mdTest = ack.reverseLookup('markdown');
 assert.equal(mdTest[0], 'md');
 assert.equal(mdTest[1], 'markdown');
 assert.equal(mdTest.length, 2);
+var pyTest = ack.reverseLookup('python')
+assert.equal(pyTest[0], 'py')
